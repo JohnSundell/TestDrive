f3390e0d681393eae4cc510ce6a116ffb36c2f0f
John Sundell <john@sundell.co>
2017-04-10 00:29:09 +0200

    Add dependencies to Package/Marathonfile + generate main.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,25 @@
-// swift-tools-version:3.1
+/**
+ *  TestDrive
+ *  Copyright (c) John Sundell 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
 
+import Foundation
 import PackageDescription
 
+// Copy the script into `main.swift` to build a command line tool
+let scriptURL = URL(fileURLWithPath: "Sources/TestDrive.swift")
+let scriptData = try Data(contentsOf: scriptURL)
+
+let mainURL = URL(fileURLWithPath: "Sources/main.swift")
+try scriptData.write(to: mainURL)
+
 let package = Package(
-    name: "TestDrive"
+    name: "TestDrive",
+    dependencies: [
+        .Package(url: "git@github.com:JohnSundell/Xgen.git", majorVersion: 1),
+        .Package(url: "git@github.com:JohnSundell/Files.git", majorVersion: 1),
+        .Package(url: "git@github.com:JohnSundell/ShellOut.git", majorVersion: 1)
+    ],
+    exclude: ["Sources/TestDrive.swift"]
 )
